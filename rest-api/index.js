a6c3b25ebdd6a79d3bbb99b7c49c20c4e1c089c8
Willy Wakam <williamswakam09@gmail.com>
2024-11-14 19:51:57 +0100

    update

--- rest-api/index.js
+++ rest-api/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
-    origin: 'https://wnw-app.vercel.app/',
+    origin: '*',
     allowOrigin: true,
 }));
 
