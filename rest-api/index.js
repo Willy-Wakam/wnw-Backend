9a1f5eba67c4df577eebc6d1639c3001e46b23fc
Willy Wakam <williamswakam09@gmail.com>
2024-11-14 19:24:58 +0100

    Update the frontend-url

--- rest-api/index.js
+++ rest-api/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
-    origin: 'https://wnw-app.vercel.app',
+    origin: 'https://vercel.com/willys-projects-eee5ca08/wnw-app/97WVa9ThSMuxTvW1Fm25o8GzZUQp',
     allowOrigin: true,
 }));
 
